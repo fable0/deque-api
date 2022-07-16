caf92d7dd0e7f78f48dbb488674fe4878d0be902
DesnoError <desnoerror@gmail.com>
2022-07-16 14:43:28 -0500

    removed redundant public modifier

--- deque/ArrayDeque.java
+++ deque/ArrayDeque.java
@@ -178,7 +178,7 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
     private class ArrayDequeIterator implements Iterator<T> {
         private int pos;
 
-        public ArrayDequeIterator() {
+        ArrayDequeIterator() {
             pos = firstIndex();
         }
         @Override
